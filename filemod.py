146cf1b3d34fdd6d4f245c1f7d75fc1aa2ac036e
Vaibhav Saxena <vbhvsxn@gmail.com>
2017-01-23 22:50:08 +0530

    Add files via upload
    code for modification and modified tunes file

---
+++ filemod.py
@@ -0,0 +1,18 @@
+fp=open(r'C:\Users\vaibhav\Desktop\tunes.txt')
+gp=open(r'C:\Users\vaibhav\Desktop\tunesfinal.txt','w')
+line=list()
+while 1:
+    l=fp.readline()
+    line.append(l)
+    if not l:
+        break
+for l in line:
+    if len(l)>1:
+        #if l[0] == 'X' and l[1] == ':':
+         #   gp.write('\n')
+          #  continue
+        if (ord(l[0])>=65 and ord(l[0])<=90 and l[1]==':') or l[0]=='%' :
+            continue
+    print(l)
+    gp.write(l)
+
